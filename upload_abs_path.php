4ae705c316d05c35a7c23997610460ae0945f929
Thorbjörn Backlund <thorbjorn@spiro.se>
2018-04-29 13:34:35 +0200

    old v0.0.2

---
+++ upload_abs_path.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @package UploadAbsPath
+ * @author Puggan
+ * @version 0.0.2
+ */
+
+/*
+Plugin Name: UploadAbsPath
+Description: Makes Wordpress able to accept absolute paths as uploads dirs
+Version: 0.0.2
+Author: Puggan
+Author URI: http://blog.puggan.se
+*/
+
+DEFINE("UPLAOD_ABS_PATH_PLUGIN_VERSION",'0.0.1');
+
+add_filter('upload_dir', 'upload_abs_path_fix');
+
+function upload_abs_path_fix($param)
+{
+	if(defined( 'UPLOADS' ))
+	{
+		if(substr(UPLOADS, 0, 1) == '/')
+		{
+			$param['path'] = str_replace(ABSPATH . UPLOADS, UPLOADS, $param['path']);
+			$param['basedir'] = str_replace(ABSPATH . UPLOADS, UPLOADS, $param['basedir']);
+		}
+
+		if(defined( 'UPLOADS_URL' ))
+		{
+			$param['url'] = str_replace(UPLOADS, UPLOADS_URL, $param['url']);
+			$param['baseurl'] = str_replace(UPLOADS, UPLOADS_URL, $param['baseurl']);
+
+			if(substr(UPLOADS_URL, 0, 1) == '/')
+			{
+				$siteurl = trailingslashit(get_option( 'siteurl' ));
+				$site_url_parts = parse_url($siteurl);
+				$site_url_root = "{$site_url_parts['scheme']}://{$site_url_parts['host']}" . ($site_url_parts['port'] ? ":{$site_url_parts['port']}" :'');
+
+				$param['url']     = str_replace($siteurl, $site_url_root, $param['url']);
+				$param['baseurl'] = str_replace($siteurl, $site_url_root, $param['baseurl']);
+			}
+		}
+	}
+
+	return $param;
+}
+
